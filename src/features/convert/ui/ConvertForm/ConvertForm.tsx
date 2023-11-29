c587a8471971c77da89503ae281bf21204544893
efValeron <masterbox06@gmail.com>
2023-11-29 14:26:04 +0600

    Add debouncing to form submissions and improve UI. Chakra-ui replaced with Antd
    Refactored the currency conversion form in the app to include debouncing (which prevents the form from being submitted too often). This was made possible through lodash. The user interface was also improved by integrating ant-design, which changed the button, select and input elements for enhanced visual representation and performance. The CurrencyRatesSection was temporarily commented out and additional padding was added to the application for improved appearance.

--- src/features/convert/ui/ConvertForm/ConvertForm.tsx
+++ src/features/convert/ui/ConvertForm/ConvertForm.tsx
@@ -1,9 +1,12 @@
-import { SubmitHandler, useForm } from "react-hook-form"
+import { Controller, useForm } from "react-hook-form"
 import S from "./ConvertForm.module.scss"
 import { CurrencyCodes } from "@/common/constants"
 import { useLazyConvertQuery } from "@/features/convert/api"
-import { useEffect } from "react"
+import { useCallback, useEffect } from "react"
 import { useConvertQueryParams } from "@/features/convert/lib"
+import { Button, Input, Select } from "antd"
+import { LoadingOutlined, SwapOutlined } from "@ant-design/icons"
+import { debounce } from "lodash"
 
 export type Inputs = {
   from: string
@@ -15,87 +18,126 @@ export const ConvertForm = () => {
   const [trigger, result] = useLazyConvertQuery()
   const { queryTo, queryFrom, queryAmount, setParams } = useConvertQueryParams()
 
-  useEffect(() => {
-    trigger({ from: queryFrom, to: queryTo, amount: queryAmount.toString() })
-  }, [queryAmount, queryFrom, queryTo, trigger])
-
   const {
-    register,
-    handleSubmit,
+    watch,
     formState: { errors },
     setValue,
     getValues,
+    control,
   } = useForm<Inputs>({ defaultValues: { from: queryFrom, to: queryTo, amount: queryAmount } })
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log("sending")
+  const debouncedHandleSubmit = useCallback(
+    debounce((formData: Inputs) => {
+      setParams(formData.from, formData.to, formData.amount.toString())
+    }, 500), // <== TIMEOUT IS HERE
+    [],
+  )
 
-    const payload = {
-      from: data.from,
-      to: data.to,
-      amount: data.amount.toString(),
-    }
-
-    setParams(data.from, data.to, data.amount.toString())
-    trigger(payload)
-  }
+  const handleSubmit = useCallback(
+    (formData: Inputs) => {
+      setParams(formData.from, formData.to, formData.amount.toString())
+    },
+    [setParams],
+  )
 
   const swapCurrencies = () => {
     const [from, to] = [getValues("from"), getValues("to")]
-    console.log(`Swapping: from ${to}, to ${from}`)
     setValue("from", to)
     setValue("to", from)
   }
 
+  useEffect(() => {
+    trigger({ from: queryFrom, to: queryTo, amount: queryAmount.toString() })
+  }, [queryAmount, queryFrom, queryTo, trigger])
+
+  useEffect(() => {
+    watch((value, { name }) => {
+      if (name === "amount") {
+        debouncedHandleSubmit(value as Inputs)
+      } else {
+        handleSubmit(value as Inputs)
+      }
+    })
+    return () => {
+      debouncedHandleSubmit.cancel()
+    }
+  }, [debouncedHandleSubmit, handleSubmit, watch])
+
+  const swapButtonDisabled = result.isFetching || result.isLoading || watch("from") === watch("to")
+
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)} className={S.form}>
-        <div className={S.formColumn}>
-          <div className={S.formItem}>
-            <select {...register("from", { required: true })}>
-              {Object.keys(CurrencyCodes).map((currencyCode) => (
-                <option key={currencyCode} value={currencyCode}>
-                  {currencyCode} {CurrencyCodes[currencyCode]}
-                </option>
-              ))}
-            </select>
-            {errors.from?.type === "required" && <span>This field is required</span>}
-          </div>
-
-          <div className={S.formItem}>
-            <input type="number" step="0.01" {...register("amount", { required: true, min: 0.01 })} />
-            {errors.amount?.type === "required" && <span>This field is required</span>}
-            {errors.amount?.type === "min" && <span>Minimum amount is 0.01</span>}
-          </div>
+    <form className={S.form}>
+      <div className={S.formColumn}>
+        <div className={S.formItem}>
+          <Controller
+            name="from"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <>
+                <Select {...field} showSearch size="large">
+                  {Object.keys(CurrencyCodes).map((currencyCode) => (
+                    <Select.Option key={currencyCode} value={currencyCode}>
+                      {currencyCode} {CurrencyCodes[currencyCode]}
+                    </Select.Option>
+                  ))}
+                </Select>
+                {errors.from?.type === "required" && <span>This field is required</span>}
+              </>
+            )}
+          />
         </div>
 
         <div className={S.formItem}>
-          <button type="button" onClick={swapCurrencies}>
-            swap
-          </button>
+          <Controller
+            name="amount"
+            control={control}
+            rules={{ required: true, min: 0.01 }}
+            render={({ field }) => (
+              <>
+                <Input type="number" step="0.01" {...field} size="large" />
+                {errors.amount?.type === "required" && <span>This field is required</span>}
+                {errors.amount?.type === "min" && <span>Minimum amount is 0.01</span>}
+              </>
+            )}
+          />
         </div>
+      </div>
 
-        <div className={S.formColumn}>
-          <div className={S.formItem}>
-            <select {...register("to", { required: true })}>
-              {Object.keys(CurrencyCodes).map((currencyCode) => (
-                <option key={currencyCode} value={currencyCode}>
-                  {currencyCode} {CurrencyCodes[currencyCode]}
-                </option>
-              ))}
-            </select>
-            {errors.to?.type === "required" && <span>This field is required</span>}
-          </div>
+      <div className={S.formItem}>
+        <Button
+          shape="circle"
+          onClick={swapCurrencies}
+          icon={!swapButtonDisabled ? <SwapOutlined /> : <LoadingOutlined spin />}
+          disabled={swapButtonDisabled}
+        />
+      </div>
 
-          <div className={S.formItem}>
-            <input type="number" readOnly value={result.isSuccess ? result.data.result : ""} />
-          </div>
+      <div className={S.formColumn}>
+        <div className={S.formItem}>
+          <Controller
+            name="to"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <>
+                <Select {...field} showSearch size="large">
+                  {Object.keys(CurrencyCodes).map((currencyCode) => (
+                    <Select.Option key={currencyCode} value={currencyCode}>
+                      {currencyCode} {CurrencyCodes[currencyCode]}
+                    </Select.Option>
+                  ))}
+                </Select>
+                {errors.to?.type === "required" && <span>This field is required</span>}
+              </>
+            )}
+          />
         </div>
 
         <div className={S.formItem}>
-          <button type="submit">Send</button>
+          <Input size="large" type="number" readOnly value={result.isSuccess ? result.data.result : ""} />
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   )
 }
